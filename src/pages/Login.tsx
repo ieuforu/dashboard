6eaf13f6424ffb2b01c64294971d6b45c6eec4af
ieuforu <ieuforu@gmail.com>
2025-11-13 16:57:45 +0800

    fix: 修改favicon

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,11 +10,7 @@ const Login = () => {
         <div className="flex flex-col items-center bg-[var(--semi-color-bg-0)] rounded-lg shadow-[0_0_1px_0_#0000004d,0_4px_14px_0_#0000001a] px-14 py-12 w-full max-w-[552px] min-h-[546px] space-y-[30px]">
           {/* 顶部 Logo + 标题 */}
           <div className="flex flex-col items-center space-y-6">
-            <img
-              src="https://lf26-static.semi.design/obj/semi-tos/template/10ad1e1b-7ea2-47df-9a25-aa16d20ffaf7.SVG"
-              alt="logo"
-              className="w-[72px] h-[72px]"
-            />
+            <img src="/logo.png" alt="logo" className="w-[72px] h-[72px]" />
             <header className="flex flex-col items-center justify-center space-y-[6px]">
               <p className="text-[32px] leading-[44px] font-semibold text-[var(--semi-color-text-0)]">
                 欢迎回来
@@ -31,7 +27,7 @@ const Login = () => {
 
           {/* 表单部分 */}
           <div className="flex flex-col items-start w-full space-y-7">
-            <Form className="flex flex-col w-full space-y-6">
+            <Form className="flex flex-col w-full space-y-2">
               <Form.Input
                 label={{ text: '用户名' }}
                 field="username"
